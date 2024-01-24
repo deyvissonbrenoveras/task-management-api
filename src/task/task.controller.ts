94cfd2ac994737bd91ed937dfb7f7a6f59b545f6
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2024-01-23 21:35:44 -0300

    feat: find by id

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { TaskDto } from './task.dto';
 import { TaskService } from './task.service';
 
@@ -9,6 +17,11 @@ export class TaskController {
   @Post()
   create(@Body() task: TaskDto) {
     return this.taskService.create(task);
+  }
+
+  @Get('/:id')
+  findById(@Param('id') id: string) {
+    return this.taskService.findById(id)
   }
 
   @Get()
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -4,8 +4,19 @@ import { TaskDto } from './task.dto';
 @Injectable()
 export class TaskService {
   private tasks: TaskDto[] = [];
+
   create(task: TaskDto) {
     this.tasks.push(task);
+  }
+
+  findById(id: string): TaskDto {
+    const foundTask = this.tasks.filter(t => t.id === id);
+
+    if (foundTask.length) {
+      return foundTask[0]
+    }
+
+    throw new HttpException(`Task with id ${id} not found`, HttpStatus.NOT_FOUND)
   }
 
   findAll(): TaskDto[] {
