7b610592d52c3ca5aec70fcaf5b680d3ba5934c6
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2024-01-25 13:39:01 -0300

    chore: setup jwt & config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TaskModule } from './task/task.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TaskModule, AuthModule, UsersModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), TaskModule, AuthModule, UsersModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
