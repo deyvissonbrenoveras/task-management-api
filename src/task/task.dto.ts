75a4ba4d80e8c7fc6c24ba24987a25acafc13fe4
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2024-01-25 22:07:17 -0300

    feat: task dto validation

--- src/task/task.dto.ts
+++ src/task/task.dto.ts
@@ -1,8 +1,29 @@
+import { IsDateString, IsEnum, IsOptional, IsString, MaxLength, MinLength } from "class-validator";
+
+
+export enum TaskStatusEnum {
+  TO_DO = 'TO_DO',
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE = 'DONE'
+}
 export class TaskDto {
   id: string;
+
+  @IsString()
+  @MinLength(3)
+  @MaxLength(256)
   title: string;
+
+  @IsString()
+  @MinLength(5)
+  @MaxLength(512)
   description: string;
-  status: string;
+
+  @IsEnum(TaskStatusEnum)
+  @IsOptional()
+  status: TaskStatusEnum;
+
+  @IsDateString()
   expirationDate: Date;
 }
 
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,11 +1,14 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { FindAllParameters, TaskDto } from './task.dto';
+import { FindAllParameters, TaskDto, TaskStatusEnum } from './task.dto';
+import { v4 as uuid } from 'uuid'
 
 @Injectable()
 export class TaskService {
   private tasks: TaskDto[] = [];
 
   create(task: TaskDto) {
+    task.id = uuid()
+    task.status = TaskStatusEnum.TO_DO
     this.tasks.push(task);
   }
 
