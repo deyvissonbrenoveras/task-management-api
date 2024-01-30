b85907b1dcc5730e555dc65b551885b0a7c2dc3a
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2024-01-30 12:45:53 -0300

    fix: entities on module

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -12,7 +12,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
                 username: configService.get<string>('DB_USERNAME'),
                 password: configService.get<string>('DB_PASSWORD'),
                 database: configService.get<string>('DB_NAME'),
-                entities: [__dirname + '/entities/*.ts'],
+                entities: [__dirname + '/entities/**'],
                 migrations: [__dirname + '/migrations/*.ts'],
                 synchronize: false,
             }),
